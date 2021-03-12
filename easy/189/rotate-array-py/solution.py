3d1263d4a210727cf022873dec821efed9676142
shaversj <shaversj@gmail.com>
2021-03-12 11:50:54 -0500

    Rotate Array Solution

--- easy/189/rotate-array-py/solution.py
+++ easy/189/rotate-array-py/solution.py
@@ -1,35 +1,18 @@
 from typing import List
-import heapq
-from collections import deque
 
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        counter = 0
-        while counter != k:
-            last = nums[-1]
-            nums.pop()
-            nums.insert(0, last)
-            counter += 1
-            print(nums)
+        nums_copy = nums.copy()
 
-    def rotate_modify_array(self, nums: List[int], k: int) -> None:
-        counter = 0
-        if nums:
-            while counter != k:
-                last = nums.pop()
-                nums.insert(0, last)
-                counter += 1
-                print(nums)
+        for idx in range(0, len(nums_copy)):
+            num_from_input = nums_copy[idx]
+            new_idx_location = (k + idx) % len(nums_copy)
+            nums[new_idx_location] = num_from_input
 
-            print(nums)
+        return nums
 
 
 c = Solution()
-c.rotate([1, 2, 3, 4, 5, 6, 7], 3)
-c.rotate_modify_array([], 1)
-# Input: nums = [1,2,3,4,5,6,7], k = 3
-# Output: [5,6,7,1,2,3,4]
+
+print(c.rotate([1, 2, 3, 4, 5, 6, 7], 1))
